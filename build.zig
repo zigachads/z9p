028e17b1285c081645fb8c4b3e902790d4122f55
Theo Paris <theop@flowtr.dev>
2024-02-05 22:43:56 -0800

    build: add zig module

--- build.zig
+++ build.zig
@@ -2,16 +2,22 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
+
+    const module = b.addModule("z9p", .{
+        .root_source_file = .{ .path = "src/9p.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const exe = b.addExecutable(.{
         .name = "z9p",
-
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+
+    exe.root_module.addImport("z9p", module);
 
     b.installArtifact(exe);
 
